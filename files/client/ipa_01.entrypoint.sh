1f0e1ea795b1c66b25e27ea316089b9863837bd6
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-03-27 12:30:04 +0200

    Split env vars into multiples to limit visibility where not needed, use srv-ipa_01 service user to enroll account to prevent unnecessary admin password sharing

--- files/client/ipa_01.entrypoint.sh
+++ files/client/ipa_01.entrypoint.sh
@@ -8,7 +8,7 @@ ipa_01_poweroff() {
 trap 'ipa_01_poweroff' ERR;
 
 IPA_01_CLIENT_FLAGS="${IPA_01_CLIENT_FLAGS:---no-ntp --force-join}";
-for required_env_var in IPA_01_SERVER_HOSTNAME IPA_01_DOMAIN IPA_01_ADMIN_PASSWORD; do
+for required_env_var in IPA_01_SERVER_HOSTNAME IPA_01_DOMAIN IPA_01_ENROLL_USER_PASSWORD; do
     if [ "${!required_env_var}" == "" ]; then
         echo "Failed to find value for '${required_env_var}', failing";
         exit 1;
@@ -20,7 +20,7 @@ for file in /ipa_01.conf.d/pre/*.sh; do
 done;
 
 # shellcheck disable=SC2086 # Client flags are intentionally without quotes.
-ipa-client-install --unattended --server "${IPA_01_SERVER_HOSTNAME}" --domain "${IPA_01_DOMAIN,,}" --principal "admin@${IPA_01_DOMAIN^^}" --password "${IPA_01_ADMIN_PASSWORD}" ${IPA_01_CLIENT_FLAGS};
+ipa-client-install --unattended --server "${IPA_01_SERVER_HOSTNAME}" --domain "${IPA_01_DOMAIN,,}" --principal "srv-ipa_01@${IPA_01_DOMAIN^^}" --password "${IPA_01_ENROLL_USER_PASSWORD}" ${IPA_01_CLIENT_FLAGS};
 
 for file in /ipa_01.conf.d/post/*.sh; do
     [[ -x "${file}" ]] && bash "${file}";
